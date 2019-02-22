2db48d4f03aafaf483493a5c3a4add5399d7848a
philipp <philipp.andelfinger@tum-create.edu.sg>
2019-02-22 08:43:31 +0800

    call correct script

--- scripts/run_all_eval.sh
+++ scripts/run_all_eval.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
+exit 1
+
 echo executing epoch length experiment
 ./scripts/epoch_length/run_epoch_length_eval.sh
 
 echo executing garbage bits experiment
 ./scripts/garbage_bits/run_gb_eval.sh
 
-echo executing running time experiment
-./scripts/running_time/run_running_time_eval.pl
-
 echo executing input transition experiment
 ./scripts/velocity_ins/run_velocity_ins_eval.sh
+
+echo executing running time experiment
+./scripts/running_time/run_running_time_eval.pl
 
 echo executing reverse exploration experiment
 ./scripts/exploration/run_exploration_eval.sh
@@ -19,6 +21,6 @@ echo executing case study experiment
 ./scripts/case_study/run_case_study_eval.sh
 
 echo executing table size experiment
-./scripts/table_size/table_size_eval.pl
+./scripts/table_size/run_table_size_eval.pl
 
 ls *.eps
