61059ac2f6319b7ca21733ced471b02ae9b1cdd1
Philipp Andelfinger <philipp.andelfinger@tum-create.edu.sg>
2019-03-15 16:01:34 +0800

    fix script

--- scripts/case_study_forward/run_case_study_fw_eval.sh
+++ scripts/case_study_forward/run_case_study_fw_eval.sh
@@ -3,10 +3,10 @@
 for granularity in 32768 16384 8192; do
   echo granularity $granularity
   echo reverse exploration
-  time ( ./main 3 1 -1 `expr 65536 \* 40` 32768 32768 0.1 2 2 10 2>&1 | tee out_bw | tail -n2)
+  time ( ./main 3 1 -1 `expr 65536 \* 40` $granularity $granularity 0.1 2 2 10 2>&1 | tee out_bw | tail -n2)
   
   echo forward exploration
-  time ( ./main 4 1 -1 `expr 65536 \* 40` 32768 32768 0.1 2 2 10 2>&1 | tee out_fw | tail -n2)
+  time ( ./main 4 1 -1 `expr 65536 \* 40` $granularity $granularity 0.1 2 2 10 2>&1 | tee out_fw | tail -n2)
   
   ./scripts/case_study_forward/compare_fw_bw.pl out_fw out_bw
 done
